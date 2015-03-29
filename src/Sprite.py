2f3205af42af1afad3b5a11032a96697efaeea14
jonem3 <matt.phil.jones@gmail.com>
2015-03-29 18:37:56 +0100

    Prevented Sprite from running away(off the edge of the screen)

--- src/Sprite.py
+++ src/Sprite.py
@@ -3,7 +3,7 @@ import pygame
 class Sprite:
    
     step = 5
-   
+    radius = 5
     def __init__(self,  x,  y,  screen):
         self.x = x
         self.y = y
@@ -11,28 +11,32 @@ class Sprite:
         self.draw_sprite()
         
     def move_north(self):
-        self.rm_sprite()
-        self.y -= Sprite.step
-        self.draw_sprite()
+        if self.y > self.radius:
+            self.rm_sprite()
+            self.y -= Sprite.step
+            self.draw_sprite()
         
     def move_south(self):
-        self.rm_sprite()
-        self.y += Sprite.step
-        self.draw_sprite()
+        if self.y <  self.screen.get_height() - self.radius:
+            self.rm_sprite()
+            self.y += Sprite.step
+            self.draw_sprite()
         
     def move_east(self):
-        self.rm_sprite()
-        self.x += Sprite.step
-        self.draw_sprite()
+        if self.x < self.screen.get_width() - self.radius:
+            self.rm_sprite()
+            self.x += Sprite.step
+            self.draw_sprite()
         
     def move_west(self):
-        self.rm_sprite()
-        self.x -= Sprite.step
-        self.draw_sprite()
+        if self.x > self.radius:
+            self.rm_sprite()
+            self.x -= Sprite.step
+            self.draw_sprite()
         
     def rm_sprite(self):
-        pygame.draw.circle(self.screen,  (255, 255, 255), (self.x, self.y), 10, 2)
+        pygame.draw.circle(self.screen,  (255, 255, 255), (self.x, self.y), self.radius*2, 2)
         
     def draw_sprite(self):
-        pygame.draw.circle(self.screen,  (255, 0, 0), (self.x, self.y), 10, 2)
+        pygame.draw.circle(self.screen,  (255, 0, 0), (self.x, self.y), self.radius*2, 2)
         pygame.display.update()

e428fee2be960feabe0cdef40989d933b4283efc
jonem3 <matt.phil.jones@gmail.com>
2015-04-03 16:27:11 +0100

    Added Ghosts and Avatar. Proper collision detection. Score count

--- src/Sprite.py
+++ src/Sprite.py
@@ -1,45 +1,37 @@
 import pygame
-
-class Sprite:
+from pygame.sprite import Sprite
+class SuperSprite(Sprite):
    
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+    STATIONARY = -1
+    
     step = 5
-    radius = 5
-    def __init__(self,  x,  y,  screen,  colour):
-        self.x = x
-        self.y = y
+
+    def __init__(self,  name,  x,  y,  screen,  image):
+        # Call the parent class (Sprite) constructor
+        super(SuperSprite, self).__init__()
+        # Load the image
+        self.image = pygame.image.load(image).convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.name = name
         self.screen = screen
-        self.colour = colour
-        self.draw_sprite()
+        self.direction = SuperSprite.STATIONARY
         
-    def move_north(self):
-        if self.y > self.radius:
-            self.rm_sprite()
-            self.y -= Sprite.step
-            self.draw_sprite()
-        
-    def move_south(self):
-        if self.y <  self.screen.get_height() - self.radius:
-            self.rm_sprite()
-            self.y += Sprite.step
-            self.draw_sprite()
-        
-    def move_east(self):
-        if self.x < self.screen.get_width() - self.radius:
-            self.rm_sprite()
-            self.x += Sprite.step
-            self.draw_sprite()
-        
-    def move_west(self):
-        if self.x > self.radius:
-            self.rm_sprite()
-            self.x -= Sprite.step
-            self.draw_sprite()
-        
-    def rm_sprite(self):
-        pygame.draw.circle(self.screen,  (0, 0, 255), (self.x, self.y), self.radius*2, 2)
-    def draw_sprite(self):
-        pygame.draw.circle(self.screen,  self.colour, (self.x, self.y), self.radius*2, 2)
-        pygame.display.update()
-        
-    def is_at_same_coordinates(self,  other_sprite):
-        return self.x == other_sprite.x and self.y == other_sprite.y
+    def move(self):
+        if self.direction == SuperSprite.NORTH and self.rect.y > 40:
+            self.rect.y -= SuperSprite.step
+        if self.direction == SuperSprite.SOUTH and self.rect.y < self.screen.get_height() - self.rect.height:
+            self.rect.y += SuperSprite.step
+        if self.direction == SuperSprite.WEST and self.rect.x > 0:
+            self.rect.x -= SuperSprite.step
+        if self.direction == SuperSprite.EAST and self.rect.x < self.screen.get_width() - self.rect.width:
+            self.rect.x += SuperSprite.step  
+            
+    def __repr__(self):
+        return self.name + ' at ' + str(self.rect.x) + ',' + str(self.rect.y)
+
--- src/Sprite_Game.py
+++ src/Sprite_Game.py
@@ -2,67 +2,87 @@ import pygame
 from pygame.locals import *
 import sys
 import time
-from Sprite import Sprite 
+from Sprite import SuperSprite 
 import random
 
-pygame.init()
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+GHOST_COUNT = 1000
+score = 0
 
+def render_score( screen ):
+    font = pygame.font.SysFont('Calibri', 25, True, False)
+    text = font.render("Score " + str(score), True, WHITE)
+    screen.blit(text, [screen.get_width() - 100, 10])
+    
 pygame.init()
-screen = pygame.display.set_mode((640,480))
-screen.fill((0, 0, 255))
-player_sprite = Sprite(320, 240, screen, (255, 0, 0))
-comp_sprite = Sprite(0, 0, screen, (0, 255, 0))
+pygame.display.set_caption("Get all 1000 ghosts")
+screen = pygame.display.set_mode((1024,800))
+
+pacman = SuperSprite("Pacman",  320, 240, screen,"../images/PacMK1.png")
+pacman_group = pygame.sprite.Group()
+pacman_group.add(pacman)
+
+ghost_group = pygame.sprite.Group()
+for ghosts in range(0, GHOST_COUNT):
+    ghost_x = random.randint(0, screen.get_width() -24)
+    ghost_y = random.randint(40, screen.get_height() -24)
+    ghost = SuperSprite("Ghost " + str(ghosts),  ghost_x, ghost_y, screen, "../images/GhostMK1.png")
+    ghost.direction = random.randint(0, 3)
+    ghost_group.add(ghost)
+    
 clock = pygame.time.Clock()
 # Loop until the user clicks the close button.
 done = False
 
 direction = random.randint(0, 3)
-
+pygame.display.update()
 while not done:    
+    
     pygame.event.pump()
     keys=pygame.key.get_pressed()
+    pacman.direction = SuperSprite.STATIONARY
     if keys[K_LEFT]:
-        player_sprite.move_west()
+        pacman.direction = SuperSprite.WEST
+        pacman.move()
     if keys[K_RIGHT]:
-        player_sprite.move_east()
+        pacman.direction = SuperSprite.EAST
+        pacman.move()
     if keys[K_UP]:
-        player_sprite.move_north()
+        pacman.direction = SuperSprite.NORTH
+        pacman.move()
     if keys[K_DOWN]:
-        player_sprite.move_south()
+        pacman.direction = SuperSprite.SOUTH
+        pacman.move()
     if keys[K_ESCAPE]:
         done = True
-    decision = random.randint(0, 5)
-    print ("decision number is ", decision)
-    if 0 == decision:
-        direction = random.randint(0, 3)
-        print ("new direction is ",  direction)
+    
+    
 
-    if direction == 0:
-        comp_sprite.move_north()
-    if direction == 1:
-        comp_sprite.move_south()
-    if direction == 2:
-        comp_sprite.move_west()
-    if direction == 3:
-        comp_sprite.move_east()
-    done = player_sprite.is_at_same_coordinates( comp_sprite )
+    for ghost in ghost_group.sprites():
+        decision = random.randint(0, 5)
+        #print ("decision number is ", decision)
+        if 0 == decision:
+            ghost.direction = random.randint(0, 3)
+            #print ("new direction is ",  ghost.direction)
+        ghost.move()
+        
+    collisions = pygame.sprite.groupcollide(pacman_group,  ghost_group,  False,  True)
+    
+    if len(collisions) > 0:
+        score += len( collisions[pacman] )
+
+            
+    # Render everything
+    screen.fill(BLUE)
+    pacman_group.draw(screen)
+    ghost_group.draw(screen)
+    render_score(screen)  
+    pygame.display.update()  
+    
     #for event in pygame.event.get(): # User did something
     time.sleep(0.02)
-    
-    """
-    # --- Main event loop
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player_sprite.move_west()
-            if event.key == pygame.K_RIGHT:
-                player_sprite.move_east()
-            if event.key == pygame.K_UP:
-                player_sprite.move_north()
-            if event.key == pygame.K_DOWN:
-                player_sprite.move_south()
- """
+
 
 
 
